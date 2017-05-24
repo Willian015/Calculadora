4235b1ab7b4416738c1905c1bd43c19158229c81
Willian015 <willian.itapira@gmail.com>
2017-05-23 23:38:27 -0300

    Update amsg.js

--- amsg.js
+++ amsg.js
@@ -8,9 +8,45 @@ function message()
   '!eta',
   '...foda...',
   '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+  'Oiier',
+  'Aí sim',
+  '!eta',
+  '...foda...',
+  '!putasso',
+
   ];
   var randomNumber = Math.floor(Math.random()*randomconvos.length);
   
   API.sendChat(randomconvos[randomNumber]);
-  setTimeout(message, 100);
+  setTimeout(message, 120000);
 }
